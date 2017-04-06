cd9483021b8f6c55d49c274cc1ae7e7d28663317
Admin <admin@Macintosh-254.local>
2017-04-06 14:52:14 -0700

    commit 1

---
+++ parser.php
@@ -0,0 +1,68 @@
+<?php
+require 'vendor/mgp25/instagram-php/src/Instagram.php';
+// =================
+// = Configuration =
+// =================
+$username = 'maxtubular';
+$password = 'maxrr1234';
+$path_to_db_file = 'insta_accounts.sqlite';
+$path_to_file_of_results = 'results.csv';
+$path_to_file_of_userid_made = 'userids_made.csv';
+$path_to_file_of_errors = 'errors.csv';
+$path_to_file_of_private_accounts = 'private_accounts.csv';
+$debug = false;
+
+// =================
+// =   Program    =
+// =================
+$i = new Instagram($username, $password, $debug);
+$db = new SQLite3($path_to_db_file);
+
+// pid of the process
+$pid_file = fopen('insta_parser.pid', 'w+');
+fwrite($pid_file, getmypid());
+fclose($pid_file);
+
+// Files
+$fresults = fopen($path_to_file_of_results, 'a+');
+$fusersmade = fopen($path_to_file_of_userid_made, 'a+');
+$ferr = fopen($path_to_file_of_errors, 'w+');
+$fnotauth = fopen($path_to_file_of_private_accounts, 'a+');
+
+
+$count = $db->query("SELECT COUNT(*) FROM accounts WHERE Instagram_Username != 'done'");
+$total = $count->fetchArray(SQLITE3_NUM)[0];
+echo "remained: {$total}\n";
+
+$results = $db->query("SELECT Instagram_Username, Instagram_ID FROM accounts WHERE Instagram_Username != 'done'");
+
+while ($row = $results->fetchArray(SQLITE3_NUM)) {
+    try {
+        $prova = $i->login();
+    } catch (InstagramException $e) {
+        $e->getMessage();
+        print_r($e);
+    }
+    try {
+        fputcsv($fusersmade, array($row[1]));
+        $us = $i->getGeoMedia($row[1]);
+        echo "$us";
+        foreach ($us['geo_media'] as $geo_media) {
+           fputcsv($fresults, array($row[0], $row[1], $geo_media['lat'],$geo_media['lng']));
+        }
+        $db->query("UPDATE accounts SET Instagram_Username = 'done' WHERE Instagram_Username == '{$row[0]}'");
+    } catch (Exception $e) {
+        if($e->getMessage() == "Not authorized to view user\n") {
+          fputcsv($fnotauth, array($row[1]));
+        } else {
+          fputcsv($ferr, array($row[1]));
+        }
+    }
+}
+
+fclose($fresults);
+fclose($ferr);
+fclose($fnotauth);
+fclose($fusersmade);
+
+print_r("done!relauch if there are userids with errors in {$path_to_file_of_errors}");
---
+++ parser_from_csv.php
@@ -0,0 +1,78 @@
+<?php
+require 'vendor/mgp25/instagram-php/src/Instagram.php';
+// =================
+// = Configuration =
+// =================
+$username = 'maxtubular';
+$password = 'maxrr1234';
+$path_to_the_source_file = '17k_New.csv';
+$lines_to_read = 106000; // CHANGE THIS FOR DECIDE HOW MANY RECORDS TO READ (put the exact number of lines of the source file or a partial if you want to just to test it)
+$path_to_file_of_results = 'results.csv';
+$path_to_file_of_userid_made = 'userids_made.csv';
+$path_to_file_of_errors = 'errors.csv';
+$path_to_file_of_private_accounts = 'private_accounts.csv';
+$debug = false;
+
+// =================
+// =   Program    =
+// =================
+$fp = fopen('file.csv', 'a+');
+$fusersmade = fopen('file_users_made.csv', 'a+');
+$ferr = fopen('file_with_errors.csv', 'a+');
+$fnotauth = fopen('file_with_not_authorized.csv', 'a+');
+
+$accounts = fopen($path_to_the_source_file, 'r+');
+$i = new Instagram($username, $password, $debug);
+
+// READ CSV FILE
+$users = array();
+while (!feof($accounts)) {
+  array_push($users, fgetcsv($accounts, 2048));
+}
+fclose($accounts);
+
+// CHOOSE THE NUMBER OF LINES TO READ
+$counter = 0; 
+
+$users_to_read = array();
+while ($counter < $lines_to_read) {
+  array_push($users_to_read, $users[$counter]);
+  array_shift($users);
+  $counter++;
+}
+
+echo "lines to parse: ";
+echo count($users_to_read);
+echo "\n";
+
+foreach ($users_to_read as $user) {
+  try {
+      $prova = $i->login();
+  } catch (InstagramException $e) {
+      $e->getMessage();
+      print_r($e);
+  }
+  try {
+      fputcsv($fusersmade, array($user[1]));
+      echo "test";
+      $us = $i->getGeoMedia($user[1]);
+
+      foreach ($us['geo_media'] as $geo_media) {
+         fputcsv($fp, array($user[0],$user[1],$geo_media['lat'],$geo_media['lng']));
+      }
+  } catch (Exception $e) {
+      if($e->getMessage() == "Not authorized to view user\n") {
+        fputcsv($fnotauth, array($user[1]));
+      } else {
+        fputcsv($ferr, array($user[1]));
+      }
+  }
+}
+
+
+fclose($fp);
+fclose($ferr);
+fclose($fnotauth);
+fclose($fusersmade);
+
+print_r("done!relauch if there are userids with errors in {$path_to_file_of_errors}");
